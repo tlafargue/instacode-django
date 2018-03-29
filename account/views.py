599fdfccb748677ce1848c601445888fb9a8abad
Pierre Rosin <pierrerosin22@gmail.com>
2018-03-29 20:15:50 +0200

    Cette fois ci tout marche
    Il faut mettre les templates

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout,get_user_model
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from .forms import UserLoginForm, UserRegisterForm
+
 
 
 
@@ -11,7 +12,6 @@ def home(request):
 def register_view(request):
     title= "Register"
     form = UserRegisterForm(request.POST or None)
-    print('hello')
     if form.is_valid():
         user = form.save(commit=False)
         password = form.cleaned_data.get('password')
@@ -19,13 +19,13 @@ def register_view(request):
         user.save()
         new_user = authenticate(username=user.username, password=password)
         login(request, new_user)
-        return redirect('<h1> OK </h1>')
+        return redirect('account/Loggedin.html')
     return render(request, 'account/registration.html', {"form": form})
 
 
 def logout_view(request):
     logout(request)
-    return render(request, 'account/Login.html', {"form": form})
+    return render(request, 'Homepage.html')
 
 
 def login_view(request):
@@ -36,7 +36,7 @@ def login_view(request):
         password = form.cleaned_data['password']
         user = authenticate(username=username, password=password)
         login(request, user)
-        print(request.user.is_authenticate)
+        return render(request, 'account/Loggedin.html')
 
-    return render(request, 'account/Login.html',{"form":form})
+    return render(request, 'account/Login.html')
 
