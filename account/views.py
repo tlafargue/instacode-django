e90f1f3a92d7d79b17049b0570f7bb05e2ef6ed4
Pierre Rosin <pierrerosin22@gmail.com>
2018-03-29 15:40:45 +0200

    created the account app in order to authenticate
    No URL

---
+++ account/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import UserForm
+from django.views.generic import View
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'account/registration.html'
+
+    #display a blank form
+    def get(self,request):
+        form=self.form_class(None)
+        return render(request, self.template_name, {'form':form})
+
+    #process form data
+    def post(self,request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            # normalize the data (data formatted properly)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            #authenticate
+            user = authenticate(user=username,password=password)
+            if user is not None:
+                login(request,user)
+                # return to something should be dashboard
+                return redirect('#')
+        # If wrong info we redirect here
+        return redirect('#')
+
+
+
