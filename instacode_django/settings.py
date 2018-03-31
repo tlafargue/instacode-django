0d1b960dc4423cf05afa2ad0908d9166141e0600
Prosin22 <37878653+Prosin22@users.noreply.github.com>
2018-03-31 13:09:38 +0200

    Gros commit moche de Pierre
    * ajout des templates a partir du code source
    
    * creates blueprint for footer and header for an offline user
    
    * Fix all the urls to make them dynamic
    
    * creation de inscription et login sur la navbar
    
    * Created a profile page
    
    * User Changes
    
    Nav bar now has different display if you are logged in or logged out. A user can only access his profile page when he is logged in
    
    * Created the new cours app cant mirgrate yet due to rename
    
    * Fixed the Key rename problem by reseting the database
    
    New superuser needs to be created
    
    * changed login
    
    * gitignore
    
    * Update .gitignore
    
    * Delete vcs.xml
    
    * Delete workspace.xml
    
    * Delete views.cpython-36.pyc
    
    * Delete forms.cpython-36.pyc
    
    * Delete urls.cpython-36.pyc
    
    * Delete __init__.cpython-36.pyc
    
    * Delete wsgi.cpython-36.pyc
    
    * Delete urls.cpython-36.pyc
    
    * Delete settings.cpython-36.pyc
    
    * Delete __init__.cpython-36.pyc
    
    * Delete models.cpython-36.pyc
    
    * Delete apps.cpython-36.pyc
    
    * Delete admin.cpython-36.pyc
    
    * Delete __init__.cpython-36.pyc
    
    * Delete __init__.cpython-36.pyc
    
    * Delete 0001_initial.cpython-36.pyc

--- instacode_django/settings.py
+++ instacode_django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cours.apps.CoursConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'instacode_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'cours/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- instacode_django/urls.py
+++ instacode_django/urls.py
@@ -26,7 +26,10 @@ urlpatterns = [
     url(r'^login/', login_view ,name='login'),
     url(r'^logout/', logout_view ,name='logout'),
     url(r'^register/', register_view ,name='register'),
-    path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='instacode.html'), name='homepage'),    
-
+    url(r'^quoi/', views.cestquoi, name='quoi'),
+    url(r'^outils/', views.outils, name='outils'),
+    url(r'^avantage/', views.avantage, name='avantage'),
+    url(r'^mentions/', views.mention, name='mentions'),
+    url(r'^dashboard/', views.dashboard, name='dashboard'),
+    url(r'^account/', include('account.urls'))
 ]
